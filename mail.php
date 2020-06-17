247b891bb9ef9e6002be3adadad06918b3a946b8
Nazrul Islam Pabel <49112914+nazrulislampabel@users.noreply.github.com>
2020-06-17 15:39:01 +0600

    final task

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "independentnazrul@gmail.com";
+        $recipient = "your mail@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
